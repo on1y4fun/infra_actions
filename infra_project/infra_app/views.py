34cf0ac9d6f801bc2529ce2bb0271d75e3b7d9e8
Максим Чен <chenmaxim@gmail.com>
2022-09-01 21:00:54 +0500

    test workflow

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Еее у меня получилось!')
 
 
 def second_page(request):
